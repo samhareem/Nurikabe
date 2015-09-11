0e59cbc8981f6a87e797b0a7a7d2159694a0679b
samhareem <sami.harju@cs.helsinki.fi>
2015-09-11 07:27:25 +0300

    Add InputScanner class

---
+++ nurikabe/src/main/java/com/nurikabe/controller/InputScanner.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nurikabe.controller;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author samharju
+ */
+public class InputScanner {
+    private Scanner scanner;
+    
+    public InputScanner() {
+        this.scanner = new Scanner(System.in);
+    }
+    
+    public int readInt() throws Exception {
+        int returnValue = Integer.parseInt(scanner.nextLine());
+        return returnValue;
+    }
+}
