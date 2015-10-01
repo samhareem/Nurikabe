e23474efe01f04a2317352cc868bde05b178874d
samhareem <sami.harju@cs.helsinki.fi>
2015-10-01 18:39:20 +0300

    Delete text based UI and related methods and variables

--- nurikabe/src/main/java/com/nurikabe/controller/Nurikabe.java
+++ nurikabe/src/main/java/com/nurikabe/controller/Nurikabe.java
@@ -16,12 +16,10 @@ public class Nurikabe {
     private final Board gameBoard;
     private GUI gui;
     private final Reader reader;
-    private final InputScanner scanner;
     
     public Nurikabe() {
         this.gameBoard = new Board();
         this.reader = new Reader();
-        this.scanner = new InputScanner();
     }
     
     public void buildLevel(int level) {
@@ -42,61 +40,7 @@ public class Nurikabe {
     
     public void start() {
         gui = new GUI(this);
-        int level = chooseLevel();
-        setLevelFilePath("/levels/" + level + ".txt");
-        if (!setBoard()) {
-            System.out.println("Error with level file");
-            System.exit(0);
-        }
-        printBoard();
-        while (true) {   
-            System.out.println("Choose x (1-9):");
-            int x = getGridCoordinate();
-            System.out.println("Choose y (1-9):");
-            int y = getGridCoordinate();
-            if (gameBoard.getGridStatus(x - 1, y - 1) == 0) {
-                gameBoard.markGrid(x-1, y-1);
-            } else {
-                gameBoard.addMistake();
-                System.out.println("Wrong move!");
-                continue;
-            }
-            if (gameBoard.isComplete()) {
-                break;
-            }
-            printBoard();
-        }
-        System.out.println("Game won!");
-    }
-
-    private int chooseLevel() {
-        int level;
-        while (true) {
-            System.out.println("Choose level?");
-            level = getIntegerInput();
-            if (level != -1) {
-                return level;
-            } else {
-                System.out.println("Invalid level");
-            }
-        }
-    }
-    
-    private int getGridCoordinate() {
-        while (true) {
-            int ret = getIntegerInput();
-            if (checkNumber(ret)) {
-                return ret;
-            }
-        }
-    }
-    private boolean checkNumber(int x) {
-        if (x < 1 || x > 9) {
-            System.out.println("Invalid number!");
-            return false;
-        }
-        return true;
-    }
+    }    
     
     private void setLevelFilePath(String path) {
         reader.setFilePath(path);
@@ -107,28 +51,6 @@ public class Nurikabe {
         return gameBoard.setBoard(boardInfo);
     }
     
-    private int getIntegerInput() {
-        int input;
-        try {
-            input = scanner.readInt();
-        } catch (Exception noValidNumber) { return -1; }
-        return input;
-    }
-    
-   private void printBoard() {
-        for (int indexY = 0; indexY < gameBoard.getBoardSize(); indexY++) {
-            for (int indexX = 0; indexX < gameBoard.getBoardSize(); indexX++) {
-                if (gameBoard.getGridStatus(indexX, indexY) == 0 || gameBoard.getGridStatus(indexX, indexY) == 1) {
-                    System.out.print("O ");
-                } else if (gameBoard.getGridStatus(indexX, indexY) == 100) {
-                    System.out.print("X ");
-                } else {
-                    System.out.print((gameBoard.getGridStatus(indexX, indexY) - 1) + " ");
-                }
-            }
-            System.out.print("\n");
-        }
-    }
     
     public boolean checkGrid(int indexX, int indexY) {
         if (gameBoard.getGridStatus(indexX, indexY) == 0) {
